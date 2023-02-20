65c56482ee6fd2854521b8ce7dc24b3e4dc7dbb2
Li <Li@zhiqing.com>
2023-02-20 15:16:09 +0800

    first commit

---
+++ src/main/java/com/li/GitTest.java
@@ -0,0 +1,12 @@
+package com.li;
+
+/**
+ * @Author: zhiqingLi
+ * @Description:
+ * @Date: Created in 15:01 2023/2/20
+ **/
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
